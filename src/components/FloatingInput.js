df520fef46b68d7f92189e5dabaf180b16e9d241
Ray Fitzgerald <56260742+TheRayFitzgerald@users.noreply.github.com>
2023-11-16 23:16:20 +0000

    attempt to fix autofocus

--- src/components/FloatingInput.js
+++ src/components/FloatingInput.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FloatingInput = ({ onEnter }) => {
   const [inputValue, setInputValue] = useState("");
   const [placeholder, setPlaceholder] = useState("Enter a word");
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
+
+  useEffect(() => {
+    setIsMobileScreen(window.innerWidth < 640);
+  }, []);
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter" && inputValue.trim()) {
@@ -22,7 +27,7 @@ const FloatingInput = ({ onEnter }) => {
       onChange={(e) => setInputValue(e.target.value)}
       onKeyPress={handleKeyPress}
       onFocus={handleFocus}
-      autoFocus
+      autoFocus={!isMobileScreen}
       placeholder={placeholder}
       className="w-64 p-3 m-3 mt-4 bg-gray-100 focus:outline-none shadow-md rounded-md text-xl font-serif text-gray-800 text-center"
       style={{ WebkitAppearance: "none" }}
