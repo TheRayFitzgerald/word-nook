8ad322494077c8804eb57326aa1d2ec4edb28172
Ray Fitzgerald <56260742+TheRayFitzgerald@users.noreply.github.com>
2023-11-18 17:21:06 +0000

    /.

--- src/components/FloatingInput.js
+++ src/components/FloatingInput.js
@@ -15,6 +15,12 @@ const FloatingInput = ({ onEnter }) => {
     setPlaceholder("");
   };
 
+  const handleBlur = () => {
+    if (!inputValue.trim()) {
+      setPlaceholder("Enter a word");
+    }
+  };
+
   return (
     <input
       type="text"
@@ -22,6 +28,7 @@ const FloatingInput = ({ onEnter }) => {
       onChange={(e) => setInputValue(e.target.value)}
       onKeyPress={handleKeyPress}
       onFocus={handleFocus}
+      onBlur={handleBlur}
       placeholder={placeholder}
       className="w-64 p-3 m-3 mt-4 bg-gray-100 focus:outline-none shadow-md rounded-md text-xl font-serif text-gray-800 text-center"
       style={{ WebkitAppearance: "none" }}
